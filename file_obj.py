24a3dcb1c2fd06629b6a8219a16bd044f44059ba
RuJiaMBP <jiaruita519@gmail.com>
2015-06-15 18:49:19 +0800

    Big refactor
    start over

--- file_obj.py
+++ file_obj.py
@@ -2,31 +2,67 @@ import os
 import heapq
 from collections import deque
 
-class FileObj(object):
-    def __init__(self, name=None, path=None, size=None):
-        self.name = name
-        self.path = path
-        self.size = size
+class FSNode(object):
+    def __init__(self, name=None, abs_path=None, size=None):
+        self.abs_path = abs_path
+        self.name = os.path.split(abs_path)[-1] if name is None else name
+        self.size = 0 if size is None else size
+        self.children = []
 
     def __lt__(self, other):
-        return self.size > other.size
+        return self.size < other.size
+
+    def __eq__(self, other):
+        return self.size == other.size
 
     def __str__(self):
-        return 'Name: %s\tPath: %s\tSize: %d' %(self.name, self.path, self.size)
+        return 'Name: {0}\tPath: {1}\tSize: {2}'.format(self.name,
+                                                        self.abs_path,
+                                                        self.size)
 
     def __repr__(self):
-        return 'Name: %s\tPath: %s\tSize: %d' %(self.name, self.path, self.size)
+        return 'Name: {0}\tPath: {1}\tSize: {2}'.format(self.name,
+                                                        self.abs_path,
+                                                        self.size)
+
+    def get_children(self):
+        """
+        Get the list of entry names under a directory.
+        :return
+            a list of strings, each of which is the name without path
+        """
+        if not os.path.isdir(self.abs_path):
+            raise Exception(str(self.abs_path) + ' should be a dir')
+        return os.listdir(self.abs_path)
 
 
-class DirObj(FileObj):
+class DirObj(FSNode):
     pass
 
 
+class FSTree(object):
+    def __init__(self, root=None):
+        self.root = root
+
+    def traverse(self, start=None):
+        if start is None:
+            start = self.root
+        if not isinstance(start, FSNode):
+            raise Exception(str(start) + " is not a FSNode")
+        children_names = start.get_children()
+        for child in children_names:
+            start.children.append(FSNode(child))
 
 
-def get_list(path):
+
+def get_entry_names_in_dir(path):
+    """
+    Get the list of entry names under a directory.
+    :return
+        a list of strings
+    """
     if not os.path.isdir(path):
-        raise Exception(path , 'should be a dir')
+        raise Exception(str(path) + ' should be a dir')
     return os.listdir(path)
         
 
@@ -37,8 +73,12 @@ def sort_all():
         for subfile in subfiles:
             subfile = os.path.join(path, subfile)
             try:
-                files.append(FileObj(os.path.split(subfile)[1], subfile, os.path.getsize(subfile)))
-            except OSError, e:
+                files.append(FSNode(os.path.split(subfile)[1],
+                                    subfile,
+                                    os.path.getsize(subfile)
+                                    )
+                             )
+            except OSError as e:
                 errors.append(subfile)
                 
     heapq.heapify(files)
@@ -54,8 +94,12 @@ def heapify_dir(root='/'):
         for subfile in subfiles:
             subfile = os.path.join(path, subfile)
             try:
-                files.append(FileObj(os.path.split(subfile)[1], subfile, os.path.getsize(subfile)))
-            except OSError, e:
+                files.append(FSNode(os.path.split(subfile)[1],
+                                    subfile,
+                                    os.path.getsize(subfile)
+                                    )
+                             )
+            except OSError as e:
                 errors.append(subfile)               
     heapq.heapify(files)
     return files
